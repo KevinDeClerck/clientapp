513f713639260d996445dda087c220fe42488428
JasperVB3 <43533535+JasperVB3@users.noreply.github.com>
2018-09-25 16:40:19 +0200

    AddressSerice aangemaakt. Nog niet volledig.

--- ClientRepository/src/main/java/com/realdolmen/maven/clientrepository/services/AddressService.java
+++ ClientRepository/src/main/java/com/realdolmen/maven/clientrepository/services/AddressService.java
@@ -1,12 +1,44 @@
 
 package com.realdolmen.maven.clientrepository.services;
 
+import com.realdolmen.maven.clientrepository.domain.Address;
+
+import com.realdolmen.maven.clientrepository.domain.PostalCode;
+import com.realdolmen.maven.clientrepository.exceptions.NoQueryPossibleException;
+import com.realdolmen.maven.clientrepository.repositories.AddressRepository;
+import java.util.List;
+
 /**
  *
  * @author SDOAX36
  */
 public class AddressService {
     
+    private AddressRepository addressRepository;
+    
+   // private PostalCodeService postalcodeservice;
+    
+    
+     public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+     public Address findAddressById(int id) throws NoQueryPossibleException {
+         return addressRepository.findById(id);
+     }
+     
+       public List<Address> findAllAddressForClientPerson() throws NoQueryPossibleException {
+        return addressRepository.findAll();
+    }
+    
+     
+     
+     /*
+     public PostalCode getPostalCode(int PostalCode) throws NoQueryPossibleException{
+         return postalCodeService.getPostalCode(postalCode);
+     }*/
+       
+       
+       
     //Insert Address
     
     //findAddressById
@@ -15,5 +47,8 @@ public class AddressService {
     //zoek een adres die van een bepaald persoon
     
     //findAllAddressFromClientFirm(int id)
+
+   
+    }
     
-}
+
