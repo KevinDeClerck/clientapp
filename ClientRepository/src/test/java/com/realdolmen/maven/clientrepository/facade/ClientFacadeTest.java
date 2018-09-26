44b1cb4684bf6c9d7c7dd291cc18a5fc5890dee9
obarojeremiah <34315822+obarojeremiah@users.noreply.github.com>
2018-09-26 11:53:34 +0200

    Merge origin/master
    Conflicts:
    	README.md

--- ClientRepository/src/test/java/com/realdolmen/maven/clientrepository/facade/ClientFacadeTest.java
+++ ClientRepository/src/test/java/com/realdolmen/maven/clientrepository/facade/ClientFacadeTest.java
@@ -5,30 +5,71 @@
  */
 package com.realdolmen.maven.clientrepository.facade;
 
+import com.realdolmen.maven.clientrepository.domain.Klant;
+import com.realdolmen.maven.clientrepository.domain.PostalCode;
+import com.realdolmen.maven.clientrepository.exceptions.NoQueryPossibleException;
+import com.realdolmen.maven.clientrepository.repositories.AbstractRepositoryTest;
+import com.realdolmen.maven.clientrepository.repositories.FirmRepository;
+import com.realdolmen.maven.clientrepository.repositories.PersonRepository;
+import com.realdolmen.maven.clientrepository.repositories.PostalCodeRepository;
+import com.realdolmen.maven.clientrepository.services.PersonService;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  *
  * @author SDOAX36
  */
+
+@RunWith(MockitoJUnitRunner.class)
 public class ClientFacadeTest {
     
+     @Mock
+    private ClientFacade clientFacade;
+
+    @Before
+    public void init() {
+       
+        clientFacade = new ClientFacade();
+    }
+
+
     public ClientFacadeTest() {
+    
     }
 
     /**
      * Test of getAllClients method, of class ClientFacade.
      */
     @Test
-    public void testGetAllClients() {
+    public void testGetAllClients() {     
     }
 
     /**
      * Test of findClientById method, of class ClientFacade.
      */
     @Test
-    public void testFindClientById() {
+    public void testFindClientById() throws NoQueryPossibleException{
+        
+        //test van findClientById method, of class ClientFacade
+      Klant klant = null;
+      PersonRepository personRepository  = new PersonRepository();
+     
+       
+        
+        
+        FirmRepository firmRepository = new FirmRepository();
+        
+        
+      
+        
     }
     
 }
