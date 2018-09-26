83169e8d00a703af43395a321086f5b3cac19592
obarojeremiah <34315822+obarojeremiah@users.noreply.github.com>
2018-09-26 14:50:52 +0200

    clientfacade findById aanpassen

--- ClientRepository/src/test/java/com/realdolmen/maven/clientrepository/facade/ClientFacadeTest.java
+++ ClientRepository/src/test/java/com/realdolmen/maven/clientrepository/facade/ClientFacadeTest.java
@@ -5,71 +5,87 @@
  */
 package com.realdolmen.maven.clientrepository.facade;
 
+import com.realdolmen.maven.clientrepository.domain.Firm;
 import com.realdolmen.maven.clientrepository.domain.Klant;
+import com.realdolmen.maven.clientrepository.domain.Person;
 import com.realdolmen.maven.clientrepository.domain.PostalCode;
 import com.realdolmen.maven.clientrepository.exceptions.NoQueryPossibleException;
 import com.realdolmen.maven.clientrepository.repositories.AbstractRepositoryTest;
 import com.realdolmen.maven.clientrepository.repositories.FirmRepository;
 import com.realdolmen.maven.clientrepository.repositories.PersonRepository;
 import com.realdolmen.maven.clientrepository.repositories.PostalCodeRepository;
+import com.realdolmen.maven.clientrepository.services.FirmService;
 import com.realdolmen.maven.clientrepository.services.PersonService;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+
 import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.*;
 
 /**
  *
  * @author SDOAX36
  */
-
 @RunWith(MockitoJUnitRunner.class)
 public class ClientFacadeTest {
-    
-     @Mock
+
+    @Mock
     private ClientFacade clientFacade;
+
+    @Mock
+    private FirmRepository firmRepository;
+    
+    @Mock
+    
+    private PersonRepository personRepository;
+    
+    @Mock 
+    
+    private PersonService personService;
+    
+    @Mock 
+    
+    private FirmService firmService;
 
     @Before
     public void init() {
-       
+
         clientFacade = new ClientFacade();
     }
 
-
     public ClientFacadeTest() {
-    
+
     }
 
     /**
      * Test of getAllClients method, of class ClientFacade.
      */
     @Test
-    public void testGetAllClients() {     
+    public void testGetAllClients() {
+
     }
 
-    /**
-     * Test of findClientById method, of class ClientFacade.
-     */
     @Test
-    public void testFindClientById() throws NoQueryPossibleException{
-        
+    public void testFindClientById() throws NoQueryPossibleException {
+
         //test van findClientById method, of class ClientFacade
-      Klant klant = null;
-      PersonRepository personRepository  = new PersonRepository();
      
-       
+        Person person = new Person() ;
+        Firm firm = new Firm();
+        
+        when(personService.findById(1)).thenReturn(person);
+        when(firmService.findById(1)).thenReturn(firm);
         
         
-        FirmRepository firmRepository = new FirmRepository();
         
         
-      
-        
+
     }
-    
+
 }
